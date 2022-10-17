8e91be9d86c666f27796fe88211ee229b132bc81
Ian Flynn <ian.flynn@duke.edu>
2022-10-17 15:47:29 -0400

    Cell map update

--- src/main/java/cellsociety/CellMapCreator.java
+++ src/main/java/cellsociety/CellMapCreator.java
@@ -1,35 +1,24 @@
 package cellsociety;
 
-import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.List;
 
 public class CellMapCreator {
 
-  enum Status{
-    DEAD(0),
-    ALIVE(1);
-    private int statusValue;
-    Status(int value){
-      this.statusValue = value;
-    }
-    private int getStatus() {
-      return this.statusValue;
-    }
-  }
+  public static final int DEAD = 0;
+  public static final int ALIVE = 1;
 
   /**
    * Still need to add how we are going to iterate over all the cells in the file that are alive,
    * although we could just create a new cell map for every coordinate and then update it for the ones
    * that are alive
-   * @param rows
-   * @param columns
-   * @param status
    * @return
    */
-  public static HashMap<Cell, Integer> CreateMap(int rows, int columns, int status){
-
+  public static HashMap<Cell, Integer> CreateMap(List<Cell> cells){
     HashMap<Cell, Integer> CellMap = new HashMap<>();
+    for(Cell newCell : cells){
+      CellMap.put(newCell, ALIVE);
+    }
     return CellMap;
   }
-
 }
