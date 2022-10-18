118980c5fb48453cfe8009e79a16bae20bf86034
Zhiyun (David) Lu <zl303@duke.edu>
2022-10-18 17:09:50 -0400

    Cell.java error handling

--- src/main/java/cellsociety/alternativemodel/Cell.java
+++ src/main/java/cellsociety/alternativemodel/Cell.java
@@ -11,15 +11,21 @@ public abstract class Cell {
   protected int type;
   public Cell(){}
   // Count the number of a certain type of neighbours
+  // return 0 when "neighbours" is null
   protected int countNeighbour(int type, Map<Coordinate,Integer> neighbours){
-    HashMap<Coordinate,Integer> neighbours_clone = new HashMap(neighbours);
-    int counter = 0;
-    for (Map.Entry<Coordinate,Integer> entry: neighbours_clone.entrySet()) {
-      if (entry.getValue() == type){
-        counter += 1;
+    try {
+      HashMap<Coordinate,Integer> neighbours_clone = new HashMap(neighbours);
+      int counter = 0;
+      for (Map.Entry<Coordinate,Integer> entry: neighbours_clone.entrySet()) {
+        if (entry.getValue() == type){
+          counter += 1;
+        }
       }
+      return counter;
     }
-    return counter;
+    catch (NullPointerException e){
+      return 0;
+    }
   }
   // Return a cell that the current cell will be updated into
   public abstract Cell update(Map<Coordinate,Integer> neighbours);
