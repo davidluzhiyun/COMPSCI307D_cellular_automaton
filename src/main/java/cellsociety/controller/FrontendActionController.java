3819c80cace774f296abe1b4fe4cadd0a7bdee75
Arman Shekarriz <abs78@duke.edu>
2022-10-24 02:53:40 -0400

    adding methods to be filled in to change facets of frontend based on splash screen choices

--- src/main/java/cellsociety/controller/FrontendActionController.java
+++ src/main/java/cellsociety/controller/FrontendActionController.java
@@ -1,5 +1,55 @@
 package cellsociety.controller;
 
+import cellsociety.gui.GUI;
+import cellsociety.gui.GUIPropertiesLoader;
+import cellsociety.gui.SimInformationDisplay;
+
 public class FrontendActionController {
+
+  private GUI simGUI;
+  private GUIPropertiesLoader GUIprops;
+  private SimInformationDisplay simInfo;
+
+
+  /**
+   * Constructor for Frontend Action Controller, needs
+   * to be able to make the view based on the game, and therefore have instances
+   * of necessary GUI classes
+   */
+  public FrontendActionController(GUI gui) {
+    simGUI = gui;
+  }
+
+  /**
+   * Setup the game chosen in the splash screen by the user
+   */
+  public void setupGameChosen(String s) {
+
+  }
+
+  /**
+   * Set the language choice chosen by the user
+   */
+  public void setLanguageChoice() {
+
+  }
+
+  /**
+   * Set the UI styling chosen properly
+   */
+  public void setUserInterfaceStyling() {
+
+  }
+
+  /**
+   * Communicate a changed state from the backend in the model to the frontend grid
+   */
+  public void sendUpdatesToUI() {
+
+  }
+
+  /**
+   * Communiate
+   */
 
 }
---
+++ src/main/java/cellsociety/gui/SplashScreen.java
@@ -0,0 +1,2 @@
+package cellsociety.gui;public class SplashScreen {
+}
